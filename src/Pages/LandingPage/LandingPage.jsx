ca0dd41f94f441b91ee819642a48efc1c2404216
Shubham807621 <shubhammusale111@gmail.com>
2025-05-27 11:06:01 +0530

    updated landing page

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import homePic from '../../Images/Home-1.png'
 import './LandingPage.css'
+import stone_img from '../../Images/about-stone-right.svg'
 
 export default function LandingPage() {
   return (
@@ -10,7 +11,10 @@ export default function LandingPage() {
                 <h3>Never Stop</h3>
                 <h1>Exploring</h1>
                 <p>Their house is a museum where people come to see ‘em. They really are a scream the Addams Family. These days are all Happy and Free. These days are all share them with me oh baby.</p>
-
+        
+            </div>
+            <div className="stone-img">
+                <img src={stone_img} />
             </div>
             <div className="landing-page-image-wrapper">
                 <img src={homePic} alt="landing-page-pic" />
